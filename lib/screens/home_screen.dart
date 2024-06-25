df0ac1cc806707c10de890f4675e52cb6451e72f
Jawwad Abbasi <jawwadabbasi990@gmail.com>
2024-06-24 23:22:17 -0400

    greet message updated based on time.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -37,6 +37,19 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  String displayGreeting() {
+    final hour = DateTime.now().hour;
+    if (hour >= 5 && hour < 12) {
+      return 'Good Morning!';
+    } else if (hour >= 12 && hour < 17) {
+      return 'Good Afternoon!';
+    } else if (hour >= 17 && hour < 21) {
+      return 'Good Evening';
+    } else {
+      return 'Good Night';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -104,9 +117,9 @@ class _HomeScreenState extends State<HomeScreen> {
                           const SizedBox(
                             height: 8,
                           ),
-                          const Text(
-                            'Good Morning',
-                            style: TextStyle(
+                          Text(
+                            displayGreeting(),
+                            style: const TextStyle(
                                 fontSize: 25,
                                 fontWeight: FontWeight.bold,
                                 color: Colors.white),
